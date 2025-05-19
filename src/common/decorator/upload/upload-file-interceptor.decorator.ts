bcad9c575700381c9585d0dac45c93dac9edcea3
Jgone2 <jgoneit@gmail.com>
2025-05-19 11:14:17 +0900

    [refactor] update only use env for maxFileSize no use default

--- src/common/decorator/upload/upload-file-interceptor.decorator.ts
+++ src/common/decorator/upload/upload-file-interceptor.decorator.ts
@@ -2,7 +2,7 @@ import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 export function UploadFileInterceptor() {
-  const maxFileSize = Number(process.env.MAX_FILE_SIZE ?? 5 * 1024 * 1024); // default 5MB
+  const maxFileSize = Number(process.env.MAX_FILE_SIZE); // default 5MB
   return applyDecorators(
     UseInterceptors(
       FileInterceptor('file', {
